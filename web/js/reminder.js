dfdf14411358bed3b18e65e1bc796349e1a1df21
Sindhu <sindhu.h.43@gmail.com>
2015-08-13 12:13:50 -0700

    modified reminder.js file

--- web/js/reminder.js
+++ web/js/reminder.js
@@ -1,37 +1,69 @@
 var reminderFunction = function (id) {
     var idAfterSplit = id.split('-').pop();
     var idOfPanel = '#panel-' + idAfterSplit;
-    var hrsId = '#hrsID-' + idAfterSplit;
-    var minsId = "#minsID-" + idAfterSplit;
     $(id).click(function () {
         $(idOfPanel).slideToggle("slow");
         $('#cancelButton-' + idAfterSplit).click(function () {
-            $(hrsId).val("");
-            $(minsId).val("");
+            $('#dateId-'+idAfterSplit).val("");
             $(idOfPanel).slideUp("slow");
         })
         $("#timeSetButton-" + idAfterSplit).click(function () {
-            var hours = parseInt($(hrsId).val());
-            var minutes = parseInt($(minsId).val());
+            var dateAndTime =$('#dateId-'+idAfterSplit).val();
             $(idOfPanel).slideUp("slow");
-            if (isNaN(hours) || isNaN(minutes)) {
-                alert("Please enter  valid integers");
+            if (!dateAndTime.trim()) {
                 return;
             }
+            else{
+                var reminderTime = dateAndTime.replace(/[/ :\s]/g, ",").split(",");
+                var month = reminderTime[0];
+                var day = reminderTime[1];
+                var year = reminderTime[2];
+                var hours = reminderTime[3];
+                var mins = reminderTime[4];
+                var amOrPm = reminderTime[5];
+                if(amOrPm == "PM" && hours < 12){
+                    hours = parseInt(hours) + 12;
+                }
+                var userEnteredDate = new Date(parseInt(year),parseInt(month-1),parseInt(day));
+                userEnteredDate.setHours(hours);
+                userEnteredDate.setMinutes(mins);
+            }
             setTimeout(function () {
-                timerFunction(hours, minutes, idAfterSplit)
+                timerFunction(userEnteredDate, idAfterSplit)
             }, 1000);
         })
     })
 }
-var timerFunction = function (hours, minutes, id) {
-    console.log(id);
-   var name =  $('#item-'+id).text();
-    console.log("name: " +name)
-    var date = new Date();
-    var presentHour = date.getHours();
-    var presentMinute = date.getMinutes();
-    if (presentHour == hours && presentMinute == minutes) {
+var timerFunction = function (inputDate , id ) {
+    var displaytime = $("#displayTimeId-" +id);
+    var presentDate = new Date();
+    var differenceTravel = inputDate.getTime() - presentDate.getTime();
+    var YearDifferenceTravel = Math.floor(differenceTravel / 1000 / 60 / 60 / 24 / 365);
+    var diffMonths = inputDate.getMonth() - presentDate.getMonth() + (12 * (inputDate.getFullYear() - presentDate.getFullYear()));
+    var diffDays = Math.floor((differenceTravel) / (1000 * 60 * 60 * 24));
+    var diffhours = Math.floor((differenceTravel) / (1000 * 60 * 60));
+    var diffminutes = Math.floor((differenceTravel) / (1000 * 60));
+    var diffseconds = Math.floor((differenceTravel) / (1000));
+    if(diffDays > 1 ){
+        displaytime.css("display" , "block");
+        displaytime.text("time left : " +diffDays+ "days");
+    }
+    else if(diffhours >= 1 && diffDays < 1){
+        displaytime.css("display" , "block");
+        displaytime.text("time left : " +diffhours+ "hrs");
+    }
+    else if(diffhours < 1 && diffminutes > 5){
+        displaytime.css("display" , "block");
+        displaytime.text("time left : " +diffminutes+ "mins");
+    }
+    else if(diffminutes < 5){
+        displaytime.css("display" , "block");
+        displaytime.text("time left : " +diffminutes+ ":" +diffseconds);
+        if(diffseconds == 0){
+            displaytime.css("display" , "none");
+        }
+    }
+    if(YearDifferenceTravel == 0 && diffMonths == 0 && diffDays == 0 && diffhours == 0 && diffminutes == 0 && diffseconds == 0) {
         bootbox.alert ({
             title: "Reminder",
             message: "item name : " +$('#item-'+id).text(),
@@ -39,6 +71,6 @@ var timerFunction = function (hours, minutes, id) {
         return;
     }
     setTimeout(function () {
-        timerFunction(hours, minutes, id)
+        timerFunction(inputDate, id)
     }, 1000);
 }
